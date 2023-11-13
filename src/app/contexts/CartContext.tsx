2a372f8c82a9580124b32d09de0f71b852b74e88
soupetersen <soupetersen@gmail.com>
2023-11-13 00:45:22 -0300

    Feat: Add methods "addCartAmount" and "removeCartAmount"  to cart context:

--- src/app/contexts/CartContext.tsx
+++ src/app/contexts/CartContext.tsx
@@ -1,11 +1,13 @@
 import { createContext, ReactNode, useState } from "react";
-import { Product } from "../schema/products";
+import { Product, ProductCart } from "../schema/products";
 
 interface CartContextData {
-	cartItems: Product[];
+	cartItems: ProductCart[];
 	cartTotal: number;
 	addToCart: (product: Product) => void;
 	removeCartItem: (productId: number) => void;
+	addCartAmount: (productId: number) => void;
+	removeCartAmount: (productId: number) => void;
 	checkIfItemAlreadyExists: (productId: number) => boolean;
 }
 
@@ -16,17 +18,52 @@ interface CartContextProviderProps {
 export const CartContext = createContext({} as CartContextData);
 
 export function CartContextProvider({ children }: CartContextProviderProps) {
-	const [cartItems, setCartItems] = useState<Product[]>([]);
+	const [cartItems, setCartItems] = useState<ProductCart[]>([]);
 
-	const cartTotal: number = cartItems.reduce(
-		(total: number, product: Product) => {
-			return total + Number(product.price);
-		},
-		0,
-	);
+	const cartTotal: number = cartItems.reduce((acc, product) => {
+		return acc + Number(product.price) * product.amount;
+	}, 0);
 
 	function addToCart(product: Product) {
-		setCartItems((state) => [...state, product]);
+		if (checkIfItemAlreadyExists(product.id)) {
+			addCartAmount(product.id);
+			return;
+		}
+
+		const cart = {
+			...product,
+			amount: 1,
+		};
+		setCartItems((state) => [...state, cart]);
+	}
+
+	function addCartAmount(id: number) {
+		const product = cartItems.find((item) => item.id === id);
+
+		if (!product) {
+			return;
+		}
+
+		product.amount++;
+
+		setCartItems((prev) => [...prev]);
+	}
+
+	function removeCartAmount(id: number) {
+		const product = cartItems.find((item) => item.id === id);
+
+		if (!product) {
+			return;
+		}
+
+		product.amount--;
+
+		if (product.amount === 0) {
+			removeCartItem(id);
+			return;
+		}
+
+		setCartItems((prev) => [...prev]);
 	}
 
 	function removeCartItem(productId: number) {
@@ -44,6 +81,8 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
 				addToCart,
 				removeCartItem,
 				cartTotal,
+				addCartAmount,
+				removeCartAmount,
 				checkIfItemAlreadyExists,
 			}}
 		>
