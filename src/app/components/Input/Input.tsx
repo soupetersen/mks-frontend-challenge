0b17bf850b4fd76cea80b6c55e011c0528b313eb
soupetersen <soupetersen@gmail.com>
2023-11-13 00:44:31 -0300

    Feat: Add input product amout component

---
+++ src/app/components/Input/Input.tsx
@@ -0,0 +1,78 @@
+import { useCart } from "@/app/hooks/useCart";
+import { Product, ProductCart } from "@/app/schema/products";
+import styled from "styled-components";
+
+interface InputProps {
+	product: ProductCart;
+}
+
+const InputContainer = styled.div`
+	display: flex;
+	align-items: center;
+	border-radius: 4px;
+	border: 1px solid #bfbfbf;
+	padding: 0.2rem;
+	width: 97px;
+	height: 34px;
+
+	@media only screen and (min-width: 768px) {
+		height: 20px;
+		width: 50px;
+		font-size: 1.5rem;
+	}
+`;
+
+const Signal = styled.div<{ borderPosition: "Left" | "Right" }>`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	/* height: 100%; */
+	cursor: pointer;
+
+	span {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 1.5rem;
+		font-weight: 400;
+		width: 100%;
+		${(props) =>
+			props.borderPosition === "Left"
+				? "border-left: 1px solid #bfbfbf;"
+				: "border-right: 1px solid #bfbfbf;"}
+
+		@media only screen and (min-width: 768px) {
+			font-size: 0.625rem;
+			width: 15px;
+		}
+	}
+`;
+
+const Amount = styled.span`
+	width: 100%;
+	font-size: 1.25rem;
+
+	@media only screen and (min-width: 768px) {
+		font-size: 0.5rem;
+	}
+`;
+
+export const Input = ({ product }: InputProps) => {
+	const { addCartAmount, removeCartAmount } = useCart();
+
+	return (
+		<InputContainer>
+			<Signal
+				onClick={() => removeCartAmount(product.id)}
+				borderPosition="Right"
+			>
+				<span>-</span>
+			</Signal>
+			<Amount>{product.amount}</Amount>
+			<Signal onClick={() => addCartAmount(product.id)} borderPosition="Left">
+				<span>+</span>
+			</Signal>
+		</InputContainer>
+	);
+};
