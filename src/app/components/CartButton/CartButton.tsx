751ae3892d504a51671c5c37be037316c5f6befa
soupetersen <soupetersen@gmail.com>
2023-11-13 00:43:29 -0300

    Fix: fixes some components to be responsive

--- src/app/components/CartButton/CartButton.tsx
+++ src/app/components/CartButton/CartButton.tsx
@@ -7,26 +7,48 @@ import Cart from "../Cart/Cart";
 const Button = styled.button`
 	display: flex;
 	align-items: center;
-	gap: 1rem;
-	width: 90px;
-	padding: 0.75rem 1rem;
+	gap: 0.7rem;
+	width: 52px;
+	padding: 0.5rem;
 	border-radius: 8px;
 	background-color: ${({ theme }) => theme.colors.primary};
 	cursor: pointer;
+
+	> img {
+		width: 10px;
+		height: 10px;
+	}
+
+	@media only screen and (min-width: 768px) {
+		/* For everything bigger than 768px */
+		padding: 0.75rem 1rem;
+		width: 90px;
+		gap: 1rem;
+
+		> img {
+			width: 19px;
+			height: 18px;
+		}
+	}
 `;
 
 const CartAmount = styled.span`
 	font-weight: 700;
-	font-size: 1.125rem;
+	font-size: 0.75rem;
 	color: ${({ theme }) => theme.colors.black};
 	line-height: 1.37rem;
 	font-family: "Montserrat", sans-serif;
+
+	@media only screen and (min-width: 768px) {
+		/* For everything bigger than 768px */
+		font-size: 1.125rem;
+	}
 `;
 
 export const CartButton = () => {
 	const [cartOpened, setCartOpened] = useState(false);
 
-	const { cartTotal } = useCart();
+	const { cartItems } = useCart();
 
 	function handleCloseCart(event: React.MouseEvent<HTMLElement>) {
 		event.stopPropagation();
@@ -36,7 +58,7 @@ export const CartButton = () => {
 	return (
 		<Button onClick={() => setCartOpened(true)}>
 			<Image src="./cart-icon.svg" alt="cart" width={19} height={18} />
-			<CartAmount>{cartTotal}</CartAmount>
+			<CartAmount>{cartItems?.length ?? 0}</CartAmount>
 			{cartOpened && <Cart closeCart={handleCloseCart} />}
 		</Button>
 	);
--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -9,9 +9,14 @@ const PageHeader = styled.header`
 	justify-content: space-between;
 	align-items: center;
 	height: 101px;
-	padding: 1.75rem 4rem;
+	padding: 1.438rem;
 	background-color: ${theme.background.blue};
 	box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+	@media only screen and (min-width: 768px) {
+		/* For everything bigger than 768px */
+		padding: 1.75rem 4rem;
+	}
 `;
 
 const TitleContainer = styled.div`
@@ -21,17 +26,27 @@ const TitleContainer = styled.div`
 
 const MKSText = styled.h1`
 	font-weight: 600;
-	font-size: 2.5rem;
+	font-size: 2rem;
 	line-height: 1.1rem;
 	color: ${({ theme }) => theme.colors.primary};
+
+	@media only screen and (min-width: 768px) {
+		/* For everything bigger than 768px */
+		font-size: 2.5rem;
+	}
 `;
 
 const Subtitle = styled.h2`
 	font-weight: 300;
-	font-size: 1.25rem;
+	font-size: 1rem;
 	line-height: 1.1rem;
 	margin-top: 1.9rem;
 	color: ${({ theme }) => theme.colors.primary};
+
+	@media only screen and (min-width: 768px) {
+		/* For everything bigger than 768px */
+		font-size: 1.25rem;
+	}
 `;
 
 export default function Header() {
