e2474e0b7b7552276345bb825feb257daa811a52
soupetersen <soupetersen@gmail.com>
2023-11-13 00:41:59 -0300

    Feat: Add products cart

--- src/app/components/Cart/Cart.tsx
+++ src/app/components/Cart/Cart.tsx
@@ -1,4 +1,6 @@
+import { useCart } from "@/app/hooks/useCart";
 import styled from "styled-components";
+import { CartProduct } from "./CartProduct";
 
 interface CartProps {
 	closeCart: (event: React.MouseEvent<HTMLElement>) => void;
@@ -11,15 +13,141 @@ const CartContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	height: 100vh;
-	width: 486px;
+	width: 320px;
 	background-color: ${({ theme }) => theme.background.blue};
+
+	@media only screen and (min-width: 768px) {
+		/* For everything bigger than 768px */
+		width: 486px;
+	}
+`;
+
+const TitleContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+	padding: 1.5rem;
+
+	@media only screen and (min-width: 768px) {
+		padding: 2rem;
+	}
+`;
+
+const CartTitle = styled.h1`
+	font-weight: 700;
+	font-size: 1.688rem;
+	line-height: 2rem;
+	text-align: left;
+	color: ${({ theme }) => theme.colors.primary};
+	width: 80%;
+
+	@media only screen and (min-width: 768px) {
+		width: 50%;
+	}
+`;
+
+const CloseButton = styled.button`
+	all: unset;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 46px;
+	height: 46px;
+	border-radius: 50%;
+	background-color: ${({ theme }) => theme.colors.black};
+	cursor: pointer;
+
+	@media only screen and (min-width: 768px) {
+		/* For everything bigger than 768px */
+		width: 38px;
+		height: 38px;
+	}
+
+	span {
+		font-size: 2rem;
+		color: ${({ theme }) => theme.background.blue};
+
+		@media only screen and (min-width: 768px) {
+			color: ${({ theme }) => theme.colors.primary};
+			font-size: 1.5rem;
+			font-weight: 400;
+			line-height: 0.9rem;
+		}
+	}
+`;
+
+const CartItems = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 100%;
+	gap: 1.375rem;
+	margin-top: 1rem;
+	overflow-y: auto;
+	padding: 1rem 2rem;
+`;
+
+const TotalContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	width: 100%;
+	padding: 0 2rem;
+
+	> h1 {
+		font-size: 1.75rem;
+		font-weight: 700;
+		letter-spacing: 0px;
+		text-align: left;
+		color: ${({ theme }) => theme.colors.primary};
+	}
+`;
+
+const Checkout = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 97px;
+	cursor: pointer;
+	background-color: ${({ theme }) => theme.colors.black};
+
+	> h1 {
+		font-size: 1.25rem;
+		font-weight: 700;
+		letter-spacing: 0px;
+		text-align: left;
+		color: ${({ theme }) => theme.colors.primary};
+
+		@media only screen and (min-width: 768px) {
+			font-size: 1.75rem;
+		}
+	}
 `;
 
 export default function Cart({ closeCart }: CartProps) {
+	const { cartItems, cartTotal } = useCart();
+
 	return (
 		<CartContainer>
-			<h1>Cart</h1>
-			<div onClick={closeCart}>Close</div>
+			<TitleContainer>
+				<CartTitle>Carrinhos de compras</CartTitle>
+				<CloseButton onClick={closeCart}>
+					<span>X</span>
+				</CloseButton>
+			</TitleContainer>
+			<CartItems>
+				{cartItems.map((product) => (
+					<CartProduct product={product} key={product.id} />
+				))}
+			</CartItems>
+			<TotalContainer>
+				<h1>Total:</h1>
+				<h1>R${cartTotal}</h1>
+			</TotalContainer>
+			<Checkout>
+				<h1>Finalizar Compra</h1>
+			</Checkout>
 		</CartContainer>
 	);
 }
---
+++ src/app/components/Cart/CartProduct.tsx
@@ -0,0 +1,146 @@
+import { ProductCart } from "@/app/schema/products";
+import Image from "next/image";
+import styled from "styled-components";
+import { Input } from "../Input/Input";
+import { XCircle } from "@phosphor-icons/react";
+import { useCart } from "@/app/hooks/useCart";
+
+interface CartProductProps {
+	product: ProductCart;
+}
+
+const CartProductContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	position: relative;
+	/* width: 250px; */
+	height: 100%;
+	align-items: center;
+	padding: 1.1rem 1.4rem;
+	gap: 0.75rem;
+	border-radius: 8px;
+	background-color: ${({ theme }) => theme.background.white};
+
+	@media only screen and (min-width: 768px) {
+		flex-direction: row;
+		width: 100%;
+		height: 95px;
+	}
+`;
+
+const LeftBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	gap: 0;
+
+	> img {
+		width: 100%;
+		height: 95px;
+		object-fit: contain;
+	}
+
+	@media only screen and (min-width: 768px) {
+		flex-direction: row;
+		gap: 1.3rem;
+
+		> img {
+			height: 61px;
+		}
+	}
+`;
+
+const RightBox = styled.div`
+	display: flex;
+	width: 100%;
+	flex-direction: row;
+	justify-content: space-between;
+
+	@media only screen and (min-width: 768px) {
+		align-items: center;
+	}
+`;
+
+const ProductName = styled.h1`
+	display: flex;
+	justify-content: center;
+	font-size: 1rem;
+	font-weight: 400;
+	line-height: 1.1rem;
+	letter-spacing: 0px;
+	text-align: left;
+	width: 100%;
+	text-align: left;
+	color: ${({ theme }) => theme.colors.gray};
+
+	@media only screen and (min-width: 768px) {
+		width: 130px;
+		font-size: 0.8rem;
+		font-weight: 400;
+		line-height: 1rem;
+		letter-spacing: 0px;
+		justify-content: flex-start;
+	}
+`;
+
+const InputAmoutContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 100%;
+`;
+
+const Price = styled.h2`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: #373737;
+	width: 84px;
+	height: 34.81px;
+	border-radius: 5px;
+	color: #ffffff;
+	font-size: 0.875rem;
+	font-weight: 700;
+	line-height: 1rem;
+
+	@media only screen and (min-width: 768px) {
+		font-size: 0.93rem;
+		font-weight: 700;
+		line-height: 0.93rem;
+		text-align: left;
+		background-color: transparent;
+		justify-content: flex-end;
+		color: ${({ theme }) => theme.colors.black};
+	}
+`;
+
+export const CartProduct = ({ product }: CartProductProps) => {
+	const { removeCartItem } = useCart();
+
+	function removeCartProduct(id: number) {
+		removeCartItem(id);
+	}
+	return (
+		<CartProductContainer>
+			<LeftBox>
+				<Image src={product.photo} alt={product.name} width={50} height={61} />
+				<ProductName>{product.name}</ProductName>
+			</LeftBox>
+			<RightBox>
+				<InputAmoutContainer>
+					<Input product={product} />
+				</InputAmoutContainer>
+				<Price>R${Number(product.price) * product.amount}</Price>
+			</RightBox>
+			<XCircle
+				size={21}
+				color="black"
+				weight="fill"
+				style={{ position: "absolute", top: -8, right: -8 }}
+				onClick={() => removeCartProduct(product.id)}
+			/>
+		</CartProductContainer>
+	);
+};
